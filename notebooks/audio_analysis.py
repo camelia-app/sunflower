78e65df02044d8ba516324c6be1c92a94b067981
Julien Beisel <julienbeisel@gmail.com>
2021-01-16 22:27:23 +0100

    :construction: Working on the equalizer

--- notebooks/audio_analysis.py
+++ notebooks/audio_analysis.py
@@ -31,4 +31,68 @@ song.print_attributes()
 song_analyzer = SongAnalyzer(song)
 
 # %%
+def clamp(min_value, max_value, value):
+
+    if value < min_value:
+        return min_value
+
+    if value > max_value:
+        return max_value
+
+    return value
+
+
+class AudioBar:
+    def __init__(
+        self,
+        x,
+        y,
+        freq,
+        decibel,
+        color,
+        width=50,
+        min_height=10,
+        max_height=100,
+        min_decibel=-80,
+        max_decibel=0,
+    ):
+
+        self.x, self.y, self.freq = x, y, freq
+
+        self.color = color
+
+        self.width, self.min_height, self.max_height = width, min_height, max_height
+
+        self.height = min_height
+
+        self.min_decibel, self.max_decibel = min_decibel, max_decibel
+
+        self.__decibel_height_ratio = (self.max_height - self.min_height) / (
+            self.max_decibel - self.min_decibel
+        )
+
+        desired_height = decibel * self.__decibel_height_ratio + self.max_height
+
+        self.height = clamp(self.min_height, self.max_height, desired_height)
+
+
+def draw_rectangle(audiobar, frame):
+    """Draw a rectangle in the frame.
+    """
+
+    # Change (top, bottom, left, right) to your coordinates
+    left = audiobar.x
+    right = left + audiobar.width
+    bottom = 0
+    top = bottom + audiobar.height
+
+    frame[audiobar.top, left:right] = audiobar.color
+    frame[bottom, left:right] = audiobar.color
+    frame[top:bottom, left] = audiobar.color
+    frame[top:bottom, right] = audiobar.color
+
+    return frame
+
+
+# %%
 # getting a matrix which contains amplitude values according to frequency and time indexes
